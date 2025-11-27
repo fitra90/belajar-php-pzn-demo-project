e36145b3b2d8db6e1704c6e6c6003ebfbd827e21
Fitra Fadilana <fitra90@gmail.com>
2025-11-27 21:33:20 +0700

    add template view

---
+++ app/Config/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Baim\Belajar\PHP\MVC\Config;
+
+class Database
+{
+    private static array $connection = [
+        "database" => [
+            "test" => [
+                "url" => "mysql:host=localhost:3306; dbname=php_login_management_test",
+                "username" => "root",
+                "password" => "",
+            ],
+            "prod" => [
+                "url" => "mysql:host=localhost:3306; dbname=php_login_management",
+                "username" => "root",
+                "password" => "",
+
+            ]
+        ]
+    ];
+    
+    private static ?\PDO $pdo = null;
+
+    public static function getConnection(string $env = "test"): \PDO 
+    {
+        if (self::$pdo == null) {
+            //create new PDO
+            self::$pdo = new \PDO(
+                self::$connection["database"][$env]["url"],
+                self::$connection["database"][$env]["username"],
+                self::$connection["database"][$env]["password"],
+            );
+        } 
+
+        return self::$pdo;
+    }
+}
