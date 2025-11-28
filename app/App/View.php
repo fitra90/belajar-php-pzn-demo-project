acb8e1a41b4758176bdd0175db1995b7e1222f80
fitra90 <fitra90@gmail.com>
2025-11-28 22:44:17 +0700

    finish register test

--- app/App/View.php
+++ app/App/View.php
@@ -15,7 +15,10 @@ class View
     public static function redirect(string $url)
     {
         header("Location: $url");
-        exit();
+        if (getenv("mode") != "test") {
+
+            exit();
+        }
     }
 
 }
--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -1,47 +1,96 @@
 <?php
 
-namespace Baim\Belajar\PHP\MVC\Controller;
-
-use PHPUnit\Framework\TestCase;
-use Baim\Belajar\PHP\MVC\Config\Database;
-use Baim\Belajar\PHP\MVC\Repository\UserRepository;
-use Baim\Belajar\PHP\MVC\Controller\UserController;
-
-class UserControllerTest extends TestCase
-{
-    private UserController $userController;
-
-    public function testRegister()
+namespace Baim\Belajar\PHP\MVC\App {
+    function header(string $value) 
     {
-        $this->userController = new UserController();
-
-        $userRepository = new UserRepository(Database::getConnection());
-        $userRepository->deleteAll();
+        echo $value;
     }
-    
-    public function testPostRegister()
-    {
-        $this->userController->register();
+}
+
+namespace Baim\Belajar\PHP\MVC\Controller{
         
-        $this->expectOutputRegex('[Register]');
-        $this->expectOutputRegex('[Id]');
-        $this->expectOutputRegex('[Name]');
-        $this->expectOutputRegex('[Password]');
-        $this->expectOutputRegex('[Register new User]');
-    }
-    
-    public function testPostRegisterSuccess()
+    use PHPUnit\Framework\TestCase;
+    use Baim\Belajar\PHP\MVC\Config\Database;
+    use Baim\Belajar\PHP\MVC\Repository\UserRepository;
+    use Baim\Belajar\PHP\MVC\Controller\UserController;
+    use Baim\Belajar\PHP\MVC\Domain\User;
+
+    class UserControllerTest extends TestCase
     {
+        private UserController $userController;
+        private UserRepository $userRepository;
+
+        protected function setUp(): void
+        {
+            $this->userController = new UserController();
+
+            $this->userRepository = new UserRepository(Database::getConnection());
+            $this->userRepository->deleteAll();
+
+            putenv("mode=test");
+        }
         
-    }
-    
-    public function testPostRegisterValidationError()
-    {
+        public function testRegister()
+        {
+            $this->userController->register();
+            
+            $this->expectOutputRegex('[Register]');
+            $this->expectOutputRegex('[Id]');
+            $this->expectOutputRegex('[Name]');
+            $this->expectOutputRegex('[Password]');
+            $this->expectOutputRegex('[Register New User]');
+        }
+        
+        public function testPostRegisterSuccess()
+        {
+            $_POST['id'] = "eko";
+            $_POST['name'] = "Eko";
+            $_POST['password'] = "eko123";
 
-    }
-    
-    public function testPostRegisterDuplicate()
-    {
+            $this->userController->postRegister();
 
+            $this->expectOutputRegex("[Location: /users/login]");
+        }
+        
+        public function testPostRegisterValidationError()
+        {
+            $_POST['id'] = "";
+            $_POST['name'] = "Eko";
+            $_POST['password'] = "eko123";
+
+            $this->userController->postRegister();
+
+            $this->expectOutputRegex('[Register]');
+            $this->expectOutputRegex('[Id]');
+            $this->expectOutputRegex('[Name]');
+            $this->expectOutputRegex('[Password]');
+            $this->expectOutputRegex('[Register New User]');
+            $this->expectOutputRegex('[Id, name, password jangan kosong]');
+
+        }
+        
+        public function testPostRegisterDuplicate()
+        {
+            $user = new User();
+            $user->id = "eko";
+            $user->name = "Eko";
+            $user->password = "eko123";
+
+            $this->userRepository->save($user);
+
+            $_POST['id'] = "eko";
+            $_POST['name'] = "Eko";
+            $_POST['password'] = "eko123";
+
+            $this->userController->postRegister();
+
+            $this->expectOutputRegex('[Register]');
+            $this->expectOutputRegex('[Id]');
+            $this->expectOutputRegex('[Name]');
+            $this->expectOutputRegex('[Password]');
+            $this->expectOutputRegex('[Register New User]');
+            $this->expectOutputRegex('[user already exist]');
+
+        }
     }
 }
